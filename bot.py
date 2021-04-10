f686983ed07d0efae9bb76f85776348f1af17cd6
Daniel <daniel.pecherski2007@gmail.com>
2021-04-10 18:21:45 +0300

    commit 2

--- bot.py
+++ bot.py
@@ -61,5 +61,6 @@ def get_weather():
         pass
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
