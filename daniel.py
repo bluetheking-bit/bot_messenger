7f3a1b0f19aabeaf2df6ad58758ca38025eb8235
Daniel <daniel.pecherski2007@gmail.com>
2022-01-28 16:21:03 +0200

    Update changers

---
+++ daniel.py
@@ -0,0 +1,13 @@
+import turtle
+wn = turtle.screen()
+player = turtle.Turtle()
+player.shape("turtle")
+player.forward(100)
+player.left(90)
+player.forward(50)
+player.left(90)
+player.forward(100)
+player.left(90)
+player.forward(50)
+player.left(90)
+turtle.mainloop()
